52db5e325c7de9b222ffa867bbafda01f2972b6e
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-02-02 10:16:56 -0500

    day 4 data
    day4

---
+++ challenge.py
@@ -0,0 +1,8 @@
+x = "number odd or even"
+print(f"hello {x}")
+
+number = 2
+if number % 2:
+    print('even')
+else:
+    print('odd')
---
+++ jiaxi.py
@@ -0,0 +1,16 @@
+# day_of_the_week =input("what day is it")
+# if day_of_the_week == "friday":
+#     print("correct")
+# else:
+#     print("incorrect")
+
+x = "test"
+print(f"hello {x}")
+
+temp = 68
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold')
