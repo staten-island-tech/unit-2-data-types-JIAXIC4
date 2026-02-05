5255e98de8029420f4ab9842fa48778b328dc0b3
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-02-05 10:17:41 -0500

    day4
    unit 2 gcf

---
+++ GCF.py
@@ -0,0 +1,20 @@
+
+# Create a function that accepts 2 arguments. Find the greatest common factor between those numbers.
+
+number1 = int(input("enter number 1"))
+
+number2 = int(input("enter number 2"))
+
+factor1 = [1, number1]
+factor2 = [1, number2]
+
+for i in range(2,number1):
+    if(number1 % i  ==0):
+        factor1.append(i)
+
+
+for i in range(2,number2):
+    if(number2 % i  ==0):
+        factor2.append(i)
+
+print(factor1, factor2)
--- factor.py
+++ factor.py
@@ -1,6 +1,19 @@
 # Create a function that accepts an input and determines all factors of the number.
 
 number = int(input("enter a number"))
-number = str(number)
-print("1 ," + number)
+# number = str(number)
+# print("1 ," + number)
 
+# if (number % 2 > 0):
+#     print("2")
+# if(number % 3 > 0):
+#     print("3")
+
+# range = [1-100]
+factor = []
+
+for i in range(2,number):
+    if(number % i  ==0):
+        factor.append(i)
+
+print("1", factor, number)
