a8232f317cce4ecc8e6c718bbe86ac17b4f2fb10
JIAXIC4 <JIAXIC4@NYCSTUDENTS.NET>
2026-02-06 10:01:09 -0500

    final day 5 unit 2
    gcf

--- GCF.py
+++ GCF.py
@@ -1,9 +1,9 @@
 
 # Create a function that accepts 2 arguments. Find the greatest common factor between those numbers.
 
-number1 = int(input("enter number 1"))
+number1 = int(input("enter number 1: "))
 
-number2 = int(input("enter number 2"))
+number2 = int(input("enter number 2: "))
 
 factor1 = [1, number1]
 factor2 = [1, number2]
@@ -18,3 +18,14 @@ for i in range(2,number2):
         factor2.append(i)
 
 print(factor1, factor2)
+
+common = []
+
+for i in factor1:
+    if i in factor2:
+        common.append(i)
+
+gcf = str(max(common))
+print("Greatest common factor: " + gcf)
+
+    
---
+++ odd_even.py
@@ -0,0 +1,8 @@
+# Let's create a function that determines if a number is odd or even
+
+number = int(input("Enter a number: " ))
+
+if(number % 2 > 0):
+    print("odd")
+else:
+    print("even")
